9049f63fa7e552db4c667f90336591ed39c9bb2e
Valeria Ruiz Guerra <a01287333@tec.mx>
2025-08-26 14:09:16 -0600

    Update exercise.py

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,7 +1,21 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    import math
+#Entradas
+
+    area = float(input("Area a pintar en metros: "))
+
+#El área a pintar (número flotante)
+
+#La cantidad de metros cuadrados que se pueden cubrir con un litro de pintura (número flotante)
+    cobertura = float(input("Rendimiento (m2/l): "))
+
+    litros_necesarios = math.ceil(area / cobertura)
+#Salida
+
+#La cantidad de litros a pintar (número entero)
+    print("Litros a comprar:", litros_necesarios )
 
 
 if __name__ == '__main__':
